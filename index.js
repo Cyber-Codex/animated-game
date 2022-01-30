a49cb774e13276dbc0c8f2a173c1f5498f414b91
Coder.exe <97795654+Cyber-Codex@users.noreply.github.com>
2022-01-30 20:14:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+alert("Welcome to my Website");
+
+//Performing a task
+function greet(name, lastname) {
+    console.log("Hello " + name + " " + lastname);
+}
+greet("Tobias", "Hartwich");
+
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
